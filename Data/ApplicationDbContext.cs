f08131bc1558a8a09e5134143eac4ad4f831ea4b
Willian Menezes <willian_menezes_santos@hotmail.com>
2020-09-14 19:17:12 -0300

    Criado projeto inicial com crud basico de fornecedores, com acesso a banco e criação de migrations

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fornecedores.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fornecedores.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+    }
+}
---
+++ Models/TipoFornecedor.cs
@@ -0,0 +1,8 @@
+﻿namespace Fornecedores.Models
+{
+    public enum TipoFornecedor
+    {
+        PessoaFisica = 1,
+        PessoaJuridica
+    }
+}
