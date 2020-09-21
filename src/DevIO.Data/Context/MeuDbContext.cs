746e7e1aa5716a33439e82ebc43f72d818388373
Willian Menezes <willian_menezes_santos@hotmail.com>
2020-09-21 00:09:59 -0300

    Criando contexto, mepeando classes via fluentapi e criado scripts de banco de dados via console

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,36 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Linq;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //mesmo se esquecer de mapear ele entre como varchar do tamanho desejado
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+            {
+                property.Relational().ColumnType = "varchar(200)";
+            }
+
+            //busca todos as classes que herdam de entitytypeconfiguration e faz todo o mapeamento de uma vez só
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            //impedindo o delete cascade
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/DevIO.Data/Mappings/FornecedorMapping.cs
@@ -0,0 +1,34 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevIO.Data.Mappings
+{
+    public class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome)
+                    .IsRequired()
+                    .HasColumnType("varchar(200)");
+
+            builder.Property(p => p.Documento)
+                    .IsRequired()
+                    .HasColumnType("varchar(14)");
+
+            //configuracao de  1x1 fornecedor para endereço
+            builder.HasOne(x => x.Endereco)
+                    .WithOne(e => e.Fornecedor);
+
+            //configuracao de  1xN fornecedor para produtos
+            builder.HasMany(x => x.Produtos)
+                    .WithOne(p => p.Fornecedor)
+                    .HasForeignKey(p => p.FornecedorId);
+
+
+            builder.ToTable("Fornecedores");
+        }
+    }
+}
